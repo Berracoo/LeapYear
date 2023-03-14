e10f77ec960577b5cc927025d3c9550d941f3dad
CoochiecooBerrauslu <berrauslu775@gmail.com>
2023-03-14 19:57:38 +0300

    -added

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter a year : ");
+       int year = scanner.nextInt();
+        System.out.println("********************");
+
+
+        if((year % 4 == 0 && Double.valueOf(year / 4) == (int)year / 4) && (year % 100 != 0)){
+            System.out.println(year + " ->" + " is a leap year!");
+        }else if((year % 100 == 0) && (year % 400 == 0 && Double.valueOf(year / 400) == (int)year / 400)){
+            System.out.println(year + " ->" + " is a leap year!");
+        }
+        else{
+            System.out.println(year + " ->" + "isn't a leap year!");
+        }
+        // Double.valueOf(double d) is used to check whether the double value of year's quotient = integer value of year's quotient
+        // we get the integer value of year by casting it with integer.
+
+
+
+
+    }
+}
